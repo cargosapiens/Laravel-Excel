8808976fd9b99355c3232f5ae73bd23508c0283c
Rafael Pinho <rafael.ribeiro@cargosapiens.com>
2023-07-25 11:26:00 -0300

    Upgrade 8.x

---
+++ rector.php
@@ -0,0 +1,23 @@
+<?php
+
+declare(strict_types=1);
+
+use Rector\Config\RectorConfig;
+use Rector\Set\ValueObject\LevelSetList;
+
+return static function (RectorConfig $rectorConfig): void {
+    $rectorConfig->paths([
+        __DIR__ . '/src',
+        __DIR__ . '/tests',
+    ]);
+
+    // Parallel
+    $rectorConfig->parallel(3600,
+        16,
+        20);
+
+    // >= 7.3
+    $rectorConfig->sets([
+        LevelSetList::UP_TO_PHP_73
+    ]);
+};
